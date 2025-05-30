d31422ec51fedf9537d4388f601115962d78ac4d
baranbbr <baranbaburisme@hotmail.com>
2025-05-30 14:52:51 +0100

    unit tests in progress and controller tests complete

---
+++ User.Achievements.API.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using User.Achievements.API.Controllers;
+using User.Achievements.API.Models.DTOs;
+using User.Achievements.API.Services;
+
+namespace User.Achievements.API.Tests.Controllers.Test;
+
+public class ControllerTests
+{
+    private readonly Mock<ILogger<UsersController>> _loggerMock;
+    private readonly Mock<IUsersService> _usersServiceMock;
+    private readonly UsersController _controller;
+
+    public ControllerTests()
+    {
+        _loggerMock = new Mock<ILogger<UsersController>>();
+        _usersServiceMock = new Mock<IUsersService>();
+
+        _usersServiceMock.Setup(x => x.GetAllUsers())
+            .ReturnsAsync(new List<UserAchievementLevelDto>() {
+                new(1, "Bronze"),
+                new(2, "Gold")
+            });
+
+        _controller = new UsersController(_loggerMock.Object, _usersServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllUsers_Returns_NoContent_When_No_Users_Exist()
+    {
+        // Arrange
+        _usersServiceMock.Setup(x => x.GetAllUsers()).ReturnsAsync(new List<UserAchievementLevelDto>());
+
+        // Act
+        var result = await _controller.GetAllUsers();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<NoContentResult>(result);
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains("Request received to get all users.")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ),
+            Times.Once
+        );
+    }
+
+    [Fact]
+    public async Task GetAllUsers_Returns_OkResult_When_Users_Exist()
+    {
+        // Arrange
+        var expectedUsers = new List<UserAchievementLevelDto>
+        {
+            new UserAchievementLevelDto(1, "Bronze"),
+            new UserAchievementLevelDto(2, "Gold")
+        };
+        _usersServiceMock.Setup(x => x.GetAllUsers()).ReturnsAsync(expectedUsers);
+
+        // Act
+        var result = await _controller.GetAllUsers();
+
+        // Assert
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expectedUsers, okResult.Value);
+    }
+
+    [Fact]
+    public async Task GetByUserId_Returns_User_When_Exists()
+    {
+        // Arrange
+        var userId = 1;
+        var expectedUser = new UserAchievementLevelDto(userId, "Bronze");
+        _usersServiceMock.Setup(x => x.GetByUserId(userId)).ReturnsAsync(expectedUser);
+
+        // Act
+        var result = await _controller.GetByUserId(userId);
+
+        // Assert
+        Assert.NotNull(result);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expectedUser, okResult.Value);
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains($"Request received to get user by ID: {userId}")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ),
+            Times.Once
+        );
+    }
+
+    [Fact]
+    public async Task GetByUserId_Returns_NotFound_When_User_Does_Not_Exist()
+    {
+        // Arrange
+        var userId = 999; // Assuming this user does not exist
+        _usersServiceMock.Setup(x => x.GetByUserId(userId)).ReturnsAsync(new UserAchievementLevelDto(0, string.Empty));
+
+        // Act
+        var result = await _controller.GetByUserId(userId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<NotFoundResult>(result);
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains($"Request received to get user by ID: {userId}")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            ),
+            Times.Once
+        );
+    }
+}
