6409d6afaa007a2a7b07d9b7e92207528e134c2e
baranbbr <baranbaburisme@hotmail.com>
2025-06-01 20:21:37 +0100

    unit tests in progress, passing

--- User.Achievements.API/Models/User.cs
+++ User.Achievements.API/Models/User.cs
@@ -5,4 +5,19 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not User other)
+        {
+            return false;
+        }
+
+        return Id == other.Id && Name == other.Name && Email == other.Email;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name, Email);
+    }
 }
